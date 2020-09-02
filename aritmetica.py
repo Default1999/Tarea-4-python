e829a267135835ad48edb93dce835dfaf563571f
David <davidvergarapate_@hotmail.com>
2020-09-01 22:11:05 -0400

    inicio

---
+++ aritmetica.py
@@ -0,0 +1,8 @@
+#variable set
+a=4
+b=5
+c=3.4
+d=4.
+e=-2
+print(f'+ es igual a la suma, por ejemplo, si a={a} y b={b} entonces, a+b= {a+b}')
+print(f'otro ejemplo, si c={c} y e={e} entonces, c+e= {c+e}')
---
+++ diccionario.py
@@ -0,0 +1,7 @@
+#variable set
+primero = {'nombre':'Andrea','apellido': 'Zapata', 'nota de programacion': 7.0, 'nota de fisica': 6.8, 'ranking': 1}
+segundo = {'nombre':'Rodrigo','apellido': 'Lagos', 'nota de programacion': 5.0, 'nota de fisica': 5.8, 'ranking': 14}
+tercero = {'nombre':'Juan','apellido': 'Jorquera', 'nota de programacion': 4.0, 'nota de fisica': 4.5, 'ranking': 30}
+print(primero)
+print(segundo)
+print(tercero)
