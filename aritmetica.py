a7641bfc71f6d1db41736ddb8575fbd24013689d
David <davidvergarapate_@hotmail.com>
2020-09-03 15:05:19 -0400

    aritmetica casi final

--- aritmetica.py
+++ aritmetica.py
@@ -4,7 +4,24 @@ b=5
 c=3.4
 d=4.
 e=-2
+f=-1
+g=1.
+h=654543.5564332
+i=-4.6
+j=2.
+k=6
+#los f stings vacíos son a manera de separación y que los prints sean más legibles
+#prints suma
 print(f'+ es igual a la suma, por ejemplo, si a={a} y b={b} entonces, a+b= {a+b}')
-print(f'otro ejemplo, si c={c} y e={e} entonces, c+e= {c+e}')
+print(f'otro ejemplo, si c={c}, e={e} y g={g} entonces, c+e+g= {c+e+g}')
 print(f'')
+#prints multiplicación
 print(f'* corresponde a multiplicación, por lo tanto si c={c} y d={d} entonces, c*d= {c*d} ')
+print(f'otro ejemplo, si g={g}, e={e} y i={i} entonces, g*e*i= {g*e*i}')
+print(f'')
+#prints división
+print(f'/ corresponde a división, por lo tanto si j={j} y i={i} entonces, j/i= {j/i} ')
+print(f'otro ejemplo, si k={k}, c={c} y a={a} entonces, k/c/a= {k/c/a}')
+print(f'') 
+#prints potencias
+print(f'** corresponde a una potencia, por lo tanto si g={g} y h={h} entonces, g**h= {g**h} ')
