a0bf3dc87ca2803f82f360b58bd53754c194fde4
David <davidvergarapate_@hotmail.com>
2020-09-03 14:38:18 -0400

    proyecto diccionario finalizado

--- aritmetica.py
+++ aritmetica.py
@@ -6,3 +6,5 @@ d=4.
 e=-2
 print(f'+ es igual a la suma, por ejemplo, si a={a} y b={b} entonces, a+b= {a+b}')
 print(f'otro ejemplo, si c={c} y e={e} entonces, c+e= {c+e}')
+print(f'')
+print(f'* corresponde a multiplicación, por lo tanto si c={c} y d={d} entonces, c*d= {c*d} ')
--- diccionario.py
+++ diccionario.py
@@ -2,6 +2,9 @@
 primero = {'nombre':'Andrea','apellido': 'Zapata', 'nota de programacion': 7.0, 'nota de fisica': 6.8, 'ranking': 1}
 segundo = {'nombre':'Rodrigo','apellido': 'Lagos', 'nota de programacion': 5.0, 'nota de fisica': 5.8, 'ranking': 14}
 tercero = {'nombre':'Juan','apellido': 'Jorquera', 'nota de programacion': 4.0, 'nota de fisica': 4.5, 'ranking': 30}
+cuarto = {'nombre':'julia','apellido': 'higuera', 'nota de programacion': 4.9, 'nota de fisica': 5.0, 'ranking': 25}
+#print
 print(primero)
 print(segundo)
 print(tercero)
+print(cuarto)
